51e483d091c8cec8db3ab68856793f7871f48d87
vvbocko <wbocko05@gmail.com>
2026-03-20 02:10:26 +0100

    Random permutations and total distance calculation
    - Implemented `randomPermutation` using std::mt19937 to shuffle cities.
    - Added `calculateTotalDistance` to compute the full path length, including the return to the starting city.
    - Updated main loop to execute 1000 random permutations and sum the distances.

--- list1/City.cpp
+++ list1/City.cpp
@@ -3,6 +3,12 @@
 #include <vector>
 #include <string>
 #include <cmath>
+#include <algorithm>
+#include <random>
+#include <ctime>
+
+std::random_device randomDev;
+std::mt19937 gen(randomDev());
 
 struct City //jkao wierzcholek
 {
@@ -13,14 +19,14 @@ struct City //jkao wierzcholek
 
 std::vector<City> loadCitiesFile(const std::string& filename)
 {
-    std::vector<City> citiesList;
+    std::vector<City> cities;
     std::ifstream file(filename);
     std::string word;
 
     if (!file.is_open())
     {
         std::cerr << "error: " << filename << "\n";
-        return citiesList;
+        return cities;
     }
 
     while (file >> word)
@@ -34,11 +40,11 @@ std::vector<City> loadCitiesFile(const std::string& filename)
     City city;
     while (file >> city.id >> city.x >> city.y)
     {
-        citiesList.push_back(city);
+        cities.push_back(city);
     }
 
     file.close();
-    return citiesList;
+    return cities;
 }
 
 
@@ -50,11 +56,32 @@ int calculateDistance(const City& a, const City& b)
     return std::round(std::sqrt(dx * dx + dy * dy));
 }
 
+void randomPermutation(std::vector<City>& cities)
+{
+    std::shuffle(cities.begin(), cities.end(), gen);
+}
+
+int calculateTotalDistance(std::vector<City>& cities)
+{
+    int dimension = cities.size();
+    int totalDistance = 0;
+
+    for (int i=0; i<dimension-1; i++) 
+    {
+        totalDistance += calculateDistance(cities[i], cities[i+1]);
+    }
+    totalDistance += calculateDistance(cities[dimension-1], cities[0]);
+
+    return totalDistance;
+}
 
 int main()
 {
     std::string filename = "dj38.tsp"; 
     std::vector<City> cities = loadCitiesFile(filename);
+    int dimension = cities.size();
+    int sum = 0;
+    float average = 0.0f;
 
     if (!cities.empty())
     {
@@ -66,6 +93,15 @@ int main()
         std::cout << "wspolrzedne 1: " << cities[1].x << ", " << cities[1].y << "\n";
 
         std::cout << "Odleglosc miedzy mistem 0 - 1: " << distance << "\n";
+
+        
+        for (int i=0; i<1000; i++)
+        {
+            randomPermutation(cities);
+            sum += calculateTotalDistance(cities);
+        }
+        average = sum/1000.0f;
+        std::cout << "Srednia: " << average << "\n";
     }
 
     return 0;
